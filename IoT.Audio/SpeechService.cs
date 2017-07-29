dd757fbd90ca657c723617f376fd03cb63957107
Andrej Tozon <andrej@tozon.info>
2017-07-29 16:30:37 +0200

    Add project files.

---
+++ IoT.Audio/SpeechService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Globalization;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.Media.SpeechSynthesis;
+
+namespace IoT.Audio
+{
+    internal class SpeechService
+    {
+        private readonly SpeechSynthesizer speechSynthesizer;
+        private readonly MediaPlayer speechPlayer;
+
+        public SpeechService()
+        {
+            speechSynthesizer = CreateSpeechSynthesizer();
+            speechPlayer = new MediaPlayer();
+        }
+
+        private static SpeechSynthesizer CreateSpeechSynthesizer()
+        {
+            var synthesizer = new SpeechSynthesizer();
+            var voice = SpeechSynthesizer.AllVoices.SingleOrDefault(i => i.Gender == VoiceGender.Female) ?? SpeechSynthesizer.DefaultVoice;
+            synthesizer.Voice = voice;
+            return synthesizer;
+        }
+    
+        public async Task SayAsync(string text)
+        {
+            using (var stream = await speechSynthesizer.SynthesizeTextToStreamAsync(text))
+            {
+                speechPlayer.Source = MediaSource.CreateFromStream(stream, stream.ContentType);
+            }
+            speechPlayer.Play();
+        }
+
+        public bool IsQuietHoursTime()
+        {
+            var now = DateTime.Now;
+            //TODO: quiet hours
+            return now.Hour < 7 || now.Hour > 21;
+
+        }
+
+        public async Task SayTime()
+        {
+            var now = DateTime.Now;
+            var hour = now.Hour;
+
+            string timeOfDay;
+            if (hour <= 12)
+            {
+                timeOfDay = "morning";
+            }
+            else if (hour <= 17)
+            {
+                timeOfDay = "afternoon";
+            }
+            else
+            {
+                timeOfDay = "evening";
+            }
+
+            if (hour > 12)
+            {
+                hour -= 12;
+            }
+            if (now.Minute == 0)
+            {
+                await SayAsync($"Good {timeOfDay}, it's {hour} o'clock.");
+            }
+            else
+            {
+                await SayAsync($"Good {timeOfDay}, it's {hour} {now.Minute}.");
+            }
+        }
+    }
+}
---
+++ IoT.Audio/StartupTask.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading;
+using Windows.ApplicationModel.Background;
+using Windows.Media.SpeechSynthesis;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace IoT.Audio
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral deferral;
+        private Timer clockTimer;
+        private SpeechService speechService;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            deferral = taskInstance.GetDeferral();
+
+            speechService = new SpeechService();
+
+            var timeToFullHour = GetTimeSpanToNextFullHour();
+            clockTimer = new Timer(OnClock, null, timeToFullHour, TimeSpan.FromHours(1));
+
+            speechService.SayTime();
+        }
+
+        private static TimeSpan GetTimeSpanToNextFullHour()
+        {
+            var now = DateTime.Now;
+            var nextHour = new DateTime(now.Year, now.Month, now.Day, now.Hour, 0, 0).AddHours(1);
+            return nextHour - now;
+        }
+
+        private async void OnClock(object state)
+        {
+            await speechService.SayTime();
+        }
+    }
+}
